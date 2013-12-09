fe49b9017fc31509812b68ef5a6219d9ad3af641
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-08 23:11:11 -0500

    run-jsdoc function -- tested on wicked itself

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 'use strict';
 
+/** @namespace API
+ *  @desc Public wicked API
+ */
 
 /**
  * Generates jsdoc wiki pages for project of current working directory and updates github wiki with them.
  * 
  * @name wicked
+ * @memberof API
  * @function
  * @param {String} root of the project
  */
 var go = module.exports = function wicked(root) {
   
 };
+
+/** @namespace Internal
+ *  @desc Internal wicked functions
+ */
