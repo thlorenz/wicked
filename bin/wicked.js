93c2c72defd7127b209a04b29b0692d124920535
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-17 11:43:57 -0500

    ignoring .git and fixing -h flag for cmd

--- bin/wicked.js
+++ bin/wicked.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
+'use strict';
 
 var log      =  require('npmlog')
   , wicked   =  require('../')
   , minimist =  require('minimist')
   , path     =  require('path')
   , fs       =  require('fs')
+  ;
 
-function inspect(obj, depth) {
-  console.error(require('util').inspect(obj, false, depth || 5, true));
-}
+(function damnYouEsprima() {
 
 var argv = minimist(process.argv.slice(2)
   , { boolean: [ 'noclean', 'nocommit', 'h', 'help' ]
@@ -20,10 +20,12 @@ argv.loglevel = argv.loglevel || argv.l || 'info';
 if (argv.h || argv.help) {
   var usage = path.join(__dirname, 'usage.txt');
   fs.createReadStream(usage).pipe(process.stdout);
-  process.exit(0);
+  return;
 }
 
 wicked(argv, argv._, function (err) {
   if (err) return log.error('wicked', err);
   log.info('wicked', 'Everything is OK');
 });
+
+})()
